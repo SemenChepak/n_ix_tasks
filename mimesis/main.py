d8cdf9a108145b960ac36d162ddeea2d81842b3b
Semen Chepak <schepak@n-ix.com>
2021-11-04 10:31:30 +0200

    add main

--- mimesis/main.py
+++ mimesis/main.py
@@ -6,14 +6,20 @@ from csv_op import write_csv, upload_csv
 from generator import generation
 from parquet_op import spark_create_parquet, upload_directory
 
-logging.basicConfig(datefmt='%m/%d/%Y %I:%M:%S %p', filename='execution.log', filemode='w')
+if __name__ == '__main__':
+    logging.basicConfig(datefmt='%m/%d/%Y %I:%M:%S %p', filename='execution.log', filemode='w')
 
-person_number = int(input('Number of persons: '))
-logging.warning(f': script started :{os.getlogin()}, {datetime.datetime.now()}')
-list_person = generation(person_number)
-file = write_csv(list_person)
-upload_csv(file)
-print(f'Finished! uploaded file: {file.split("/")[1]}')
-spark_file = spark_create_parquet(file)
-upload_directory(spark_file)
-print(f'Finished! uploaded dir: {spark_file.split("/")[1]}')
+    person_number = int(input('Number of persons: '))
+
+    logging.warning(f': script started :{os.getlogin()}, {datetime.datetime.now()}')
+
+    list_person = generation(person_number)
+    file = write_csv(list_person)
+    upload_csv(file)
+
+    logging.info(f'Finished! uploaded file: {file.split("/")[1]}')
+
+    spark_file = spark_create_parquet(file)
+    upload_directory(spark_file)
+
+    logging.info(f'Finished! uploaded dir: {spark_file.split("/")[1]}')
