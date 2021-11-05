8e651b01895710dc90a58eedeca0019e91375931
Semen Chepak <schepak@n-ix.com>
2021-11-05 10:52:58 +0200

    using datetime module to iterate through days.

--- task_2_nbu/Extractor.py
+++ task_2_nbu/Extractor.py
@@ -1,23 +1,19 @@
 import time
+from datetime import date, timedelta, datetime
 
 import requests
 
-Currency = ['USD', 'EUR', 'MXN', 'INR', 'IRR', 'GEL']
+currency = ['USD', 'EUR', 'MXN', 'INR', 'IRR', 'GEL']
 
 
-def check_month(month: int) -> str:
-    """check month, if < 10 :return like 0month"""
-    return f'0{month}' if month < 10 else month
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
 
-def check_day(day: int) -> str:
-    """check day, if < 10 :return like 0day"""
-    return f'0{day}' if day < 10 else day
-
-
-def create_url(val: str, month: str, day: str) -> str:
+def create_url(val: str, date_f: str) -> str:
     """create url for API"""
-    return f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={val}&date=2021{month}{day}&json'
+    return f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={val}&date={date_f}&json'
 
 
 def check_res(response):
@@ -25,20 +21,20 @@ def check_res(response):
     return True if response.json() != [{"message": "Wrong parameters format"}] and len(response.json()) > 0 else False
 
 
-def get_data() -> list:
+def get_data(start_date, end_date) -> list:
     """read all results from Api from 2021/01/01, :return list_of_data"""
     data_list = []
 
-    for month in range(1, 12):
-        month_for_url = check_month(month)
-
-        for day in range(1, 31):
-            day_for_url = check_day(day)
-
-            for val in Currency:
-                response = requests.get(create_url(val, month_for_url, day_for_url))
-                if check_res(response):
-                    data_list.append(response.json()[0])
-                    print(response.json()[0])
-            time.sleep(1)
+    for single_date in daterange(start_date, end_date):
+        for val in currency:
+            response = requests.get(create_url(val, single_date.strftime("%Y%m%d")))
+            if check_res(response):
+                data_list.append(response.json()[0])
+        time.sleep(1)
     return data_list
+
+
+if __name__ == '__main__':
+    start_date = date(2021, 9, 1)
+    end_date = datetime.now().date()
+    get_data(start_date, end_date)
